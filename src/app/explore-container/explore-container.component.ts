884a0b66e364ab2e7645427c1abad2c4e0640035
AlexandreArmando <alexandrearmando@hotmail.fr>
2020-03-18 11:43:59 +0100

    validate camera plugin

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-explore-container',
@@ -8,8 +9,25 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ExploreContainerComponent implements OnInit {
   @Input() name: string;
 
-  constructor() { }
+  constructor(private camera: Camera) { }
 
   ngOnInit() {}
+
+  takePicture() {
+    const options : CameraOptions = {
+      quality : 100,
+      destinationType : this.camera.DestinationType.FILE_URI,
+      encodingType : this.camera.EncodingType.JPEG,
+      mediaType : this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      let base64Image = 'data:image/jpeg;base64,'+imageData;
+      console.log(base64Image);
+      
+    }, (err) => {
+      console.log(err);
+    })
+  }
 
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(private camera: Camera) {}
 
+  
+  
 }
